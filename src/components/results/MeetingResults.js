5b192b6b62a36ab4cb91c5e5daaf730a413c3ca3
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2019-06-26 15:27:26 +0100

    add meeting result entry, filter/map in main comp

--- src/components/results/MeetingResults.js
+++ src/components/results/MeetingResults.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react'
+import PropTypes from "prop-types";
 import CourseMeetingHeader from './CourseMeetingHeader'
+import MeetingResultEntry from './MeetingResultEntry';
 
 export class MeetingResults extends Component {
     render() {
         return (
           <div>
             {this.props.courseMeetings.map(courseMeeting => (
-                <div>
-                <CourseMeetingHeader courseMeeting={{ courseTitle: courseMeeting.course.name }} />
-                {/* ADD: Sort the props array into categories, foeach with a course meeting header  */}
+                <div key={courseMeeting.id}>
+                <CourseMeetingHeader  courseMeeting={{ courseTitle: courseMeeting.course.name }} />
+                {this.props.meetingResults
+                .filter(i=>i.courseMeetingId == courseMeeting.id)
+                .map(meetingResult => (
+                        <MeetingResultEntry
+                        key={meetingResult.id}
+                        meetingResult={meetingResult}
+                        />
+                    )
+                )}
                 </div>
             ))}
         </div>
