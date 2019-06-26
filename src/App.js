f52a9930a5f1e380f2741cd5e77dfb44ed1f9d28
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2019-06-26 14:32:05 +0100

    basic state and about page rotuing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import './App.css';
+import "./App.css";
 import Header from "./components/layout/Header";
+import About from "./components/pages/About";
 
 class App extends Component {
+  state = {
+    courseMeetings: [],
+    meetingResults: []
+  }
+
+
+
   render() {
     return (
       <Router>
         <div className="App">
           <Header />
+          <Route
+            exact
+            path="/"
+            render={props => (
+              <React.Fragment>
+                {/* add search bar under header */}
+
+
+                {/* add meeting results main component */}
+
+              </React.Fragment>
+            )}
+          />
+
+          <Route path="/about" component={About} />
         </div>
       </Router>
     );
