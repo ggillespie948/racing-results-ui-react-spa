0d65b6679277c48d1904d09c195950387f98dad0
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2019-06-26 14:43:56 +0100

    add course meeting header component

---
+++ src/components/results/CourseMeetingHeader.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+export class CourseMeetingHeader extends Component {
+    getStyle = () => {
+        return {
+          background: "#071220",
+          padding: "10px",
+          color: "white",
+          borderBottom: "1px #ccc dotted",
+        };
+      };
+  
+    render() {
+    const { id, courseTitle } = this.props.courseMeeting;
+    return (
+      <div style={this.getStyle()}>
+        <p>
+          {" "}
+          <b>{courseTitle}</b>
+          {"  "}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default CourseMeetingHeader;
