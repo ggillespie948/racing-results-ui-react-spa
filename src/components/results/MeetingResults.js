1a1121bc70deb6ac1d6e444a9d8e2ae436b0f655
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2019-06-27 16:13:18 +0100

    map meetingresults+resultentries to accordion comp

--- src/components/results/MeetingResults.js
+++ src/components/results/MeetingResults.js
@@ -1,29 +1,76 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import PropTypes from "prop-types";
-import CourseMeetingHeader from './CourseMeetingHeader'
-import MeetingResultEntry from './MeetingResultEntry';
+import CourseMeetingHeader from "./CourseMeetingHeader";
+import ResultEntry from "./ResultEntry";
+import "react-accessible-accordion/dist/fancy-example.css";
+
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel
+} from "react-accessible-accordion";
 
 export class MeetingResults extends Component {
-    render() {
-        return (
-          <div>
-            {this.props.courseMeetings.map(courseMeeting => (
-                <div key={courseMeeting.id}>
-                <CourseMeetingHeader  courseMeeting={{ courseTitle: courseMeeting.course.name }} />
-                {this.props.meetingResults
-                .filter(i=>i.courseMeetingId == courseMeeting.id)
-                .map(meetingResult => (
-                        <MeetingResultEntry
-                        key={meetingResult.id}
-                        meetingResult={meetingResult}
-                        />
-                    )
-                )}
-                </div>
-            ))}
-        </div>
-        );
-      }
-    }
+  getTableStyle = () => {
+    return {
+      background: "#06101d",
+      color: "white",
+      padding: "10px",
+      borderBottom: "1px #ccc solid",
+      width: '100%',
+      textAlign: 'center'
+    };
+  };
+  
+  render() {
+    return (
+      <div>
+        {this.props.courseMeetings.map(courseMeeting => (
+          <Accordion allowMultipleExpanded={false} allowZeroExpanded={true}>
+            <div key={courseMeeting.id}>
+              {/* Course Meeting Header */}
+              <CourseMeetingHeader
+                courseMeeting={{ courseTitle: courseMeeting.course.name }}
+              />
 
-export default MeetingResults
+              {/* Render accordion dropdown for each race at meeting */}
+              {this.props.meetingResults
+                .filter(i => i.courseMeetingId == courseMeeting.id)
+                .map(meetingResult => (
+                  <AccordionItem>
+                    <AccordionItemHeading>
+                      <AccordionItemButton>
+                        <b style={{ color: "black" }}>
+                          {meetingResult.raceTime.slice(11, 16)}
+                        </b>{" "}
+                        <a style={{ fontSize: "12px" }}>
+                          {meetingResult.raceTitle}{" "}
+                        </a>
+                      </AccordionItemButton>
+                    </AccordionItemHeading>
+                    <AccordionItemPanel>
+                      {/* Render results entry for each course meeting */}
+                        <table style={this.getTableStyle()}>
+                        <tr>
+                            <th>Place</th>
+                            <th>Name</th> 
+                            <th>Price</th>
+                        </tr>
+                        {meetingResult.resultEntries.map(resultEntry => (
+                          <ResultEntry resultEntry={resultEntry} />
+                        ))}
+                      </table>
+                    </AccordionItemPanel>
+                  </AccordionItem>
+                ))}
+            </div>
+          </Accordion>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default MeetingResults;
